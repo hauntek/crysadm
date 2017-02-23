5d6cd6752b792adb08c93192c978f60b9cf58c0f
hauntek <hauntek@hotmail.com>
2017-02-23 16:25:45 +0800

    修复并优化一个小问题

--- update.py
+++ update.py
@@ -10,9 +10,8 @@ import urllib.request
 import shutil
 import threading
 
-service_url = 'http://down.crysadmapp.cn/crysadm'
-# service_url = 'https://github.com/hauntek/crysadm/raw/master/'
-rootdir = './' # 脚本当前路径
+service_url = 'http://down.crysadmapp.cn/crysadm/'
+rootdir = '.' # 脚本当前路径
 ignore_file = ['config.py'] # 忽略文件
 
 progress = 0
@@ -24,7 +23,7 @@ def urlretrieve(url, filename):
     urllib.request.urlretrieve(url, filename) 
 
 def SnapshotW(path, cont):
-    f = open(path, "a")
+    f = open(path, 'a')
     for con in cont:
         j_con = json.dumps(con)
         r = f.write(j_con + "\n") 
@@ -43,13 +42,13 @@ def md5Checksum(filePath):
     return m.hexdigest()
 
 # 检查目录及子目录文件，生成md5校验
-def Checksum(rootdir='./', check=False):
+def Checksum(rootdir='.', check=False):
     data_list = list()
     for parent, dirnames, filenames in os.walk(rootdir):
         for filename in filenames:
             file = os.path.join(parent, filename)
             file = file.replace('\\', '/') # 路径转换
-            file = file.replace(rootdir, '') # 根目录转换
+            file = file.replace(rootdir + '/', '') # 根目录转换
 
             try:
                 md5 = md5Checksum(file)
@@ -110,7 +109,7 @@ def insp_update():
     data_file = Checksum(rootdir, False) # 是否生成本地校验记录
 
     try:
-        data = urlopen(service_url + '/filemd5.txt')
+        data = urlopen(service_url + 'filemd5.txt')
         for b_date in data:
             data_info = json.loads(b_date.decode('utf-8'))
             if data_info['file'] in ignore_file: continue
@@ -137,7 +136,7 @@ def update(backups=True):
     data_file = Checksum(rootdir, True) # 是否生成本地校验记录
 
     try:
-        data = urlopen(service_url + '/filemd5.txt')
+        data = urlopen(service_url + 'filemd5.txt')
         for b_date in data:
             data_info = json.loads(b_date.decode('utf-8'))
             if data_info['file'] in ignore_file: continue
@@ -147,13 +146,12 @@ def update(backups=True):
     except Exception as e:
         return json.dumps(dict(r='', msg='云端对比文件出现错误，请稍后重试'))
 
-    url = service_url + '/'
     if len(data_list) > 0:
         if backups:
             if os.path.exists('.backups'):
                 shutil.rmtree('.backups')
             shutil.copytree('.', '.backups')
-        threading.Thread(target=down_thread, args=(url, data_list)).start()
+        threading.Thread(target=down_thread, args=(service_url, data_list)).start()
     else:
         return json.dumps(dict(r='', msg='本地源代码和云端一致，无需更新'))
 
