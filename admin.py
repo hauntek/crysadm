4c7987080f089aeb1b5ef866602b81a2fb9c3f56
hauntek <hauntek@hotmail.com>
2017-03-27 03:43:56 +0800

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,10 +1,9 @@
-# Html － Crysadm 管理员界面
 __author__ = 'powergx'
 from flask import request, Response, session, render_template, url_for, redirect
 from crysadm import app, r_session
 from auth import requires_admin, requires_auth
-import json
 from util import hash_password
+import json
 import re
 import random
 from message import send_msg
